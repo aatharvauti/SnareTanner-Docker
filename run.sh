1b6c0f94914c5b0e35a750ede67fc245ceee1501
aatharvauti <bm.aatharvauti@protonmail.ch>
2023-08-09 22:27:59 +0530

    Mount Volume

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 redis-server --port 7777 &
 python3.9 phpox/sandbox.py &
 tanner &
-clone --target https://example.com &
-snare --port 80 --page-dir example.com --tanner 0.0.0.0 --host-ip 0.0.0.0
+# clone --target https://sakec.ac.in &
+snare --port 80 --page-dir sakec.ac.in --tanner 0.0.0.0 --host-ip 0.0.0.0
 
 # Keep the container running interactively
 exec bash
