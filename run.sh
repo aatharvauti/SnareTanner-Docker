cf9cfe170e10b9aa1e3ac06d7b86446a640391e7
aatharvauti <bm.aatharvauti@protonmail.ch>
2023-08-09 22:05:08 +0530

    example.com

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 redis-server --port 7777 &
 python3.9 phpox/sandbox.py &
 tanner &
-clone --target https://sakec.ac.in --max-depth=1 &
-snare --port 80 --page-dir sakec.ac.in --tanner 0.0.0.0
+clone --target https://example.com &
+snare --port 80 --page-dir example.com --tanner 0.0.0.0
 
 # Keep the container running interactively
 exec bash
