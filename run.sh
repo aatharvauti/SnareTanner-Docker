ff8bbe3db1fb447c829b413223d14867ad20a483
Atharva Auti <bm.aatharvauti@protonmail.ch>
2023-08-22 22:19:51 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ redis-server --port 7777 &
 python3.9 phpox/sandbox.py &
 tanner &
 clone --target https://sakec-admin.netlify.com &
-snare --port 80 --page-dir sakec-admin.netlify.com --tanner 0.0.0.0 --host-ip 0.0.0.0
 
 # Keep the container running interactively
 exec bash
